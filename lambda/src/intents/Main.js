367d2ba630badf3938f3b2f552835138d69f16fe
Andres Rodriguez <arod1198@gmail.com>
2021-02-27 15:15:36 -0500

    trying axios call

--- lambda/src/intents/Main.js
+++ lambda/src/intents/Main.js
@@ -1,23 +1,53 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MainRequestHandler = void 0;
 const ask_sdk_core_1 = require("ask-sdk-core");
 const axios_1 = require("axios");
+// let speakOutput: Promise<string>;
+const axiosCall = () => {
+    let speakOutput = 'Here\'s what you can do: ';
+    return new Promise((resolve, reject) => {
+        axios_1.default.get('https://raw.githubusercontent.com/rodriguez/personal-json-apis/main/db.json')
+            .then((response) => {
+            const body = response.data;
+            console.log(body);
+            const wordArray = body.options.map((obj) => obj.value);
+            console.log(wordArray.join(', '));
+            speakOutput = speakOutput.concat(wordArray.join(', '));
+            resolve(speakOutput);
+        }, (error) => {
+            reject('This resulted in an error.');
+            ;
+        });
+    });
+};
 exports.MainRequestHandler = {
     canHandle(handlerInput) {
         return ask_sdk_core_1.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' && ask_sdk_core_1.getIntentName(handlerInput.requestEnvelope) === 'Main';
     },
     handle(handlerInput) {
-        let speakOutput = 'Here\'s what you can do: ';
-        axios_1.default.get('https://raw.githubusercontent.com/rodriguez/personal-json-apis/main/db.json')
-            .then((response) => {
-            const body = response.data;
-            const wordArray = body.options.map((obj) => obj.value);
-            speakOutput = speakOutput.concat(wordArray.join(', '));
+        return __awaiter(this, void 0, void 0, function* () {
+            // let speakOutput = 'Here\'s what you can do: ';
+            // axios.get('https://raw.githubusercontent.com/rodriguez/personal-json-apis/main/db.json')
+            //     .then((response: AxiosResponse): void => {
+            //         const body: { options: [{ label: string, value: string }] } = response.data;
+            //         const wordArray: string[] = body.options.map((obj: { label: string, value: string }) => obj.value);
+            //         speakOutput = speakOutput.concat(wordArray.join(', '))
+            // });
+            const response = yield axiosCall();
+            return handlerInput.responseBuilder
+                .speak(response)
+                .reprompt(response)
+                .getResponse();
         });
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
     }
 };
--- lambda/ts/src/intents/Main.ts
+++ lambda/ts/src/intents/Main.ts
@@ -5,23 +5,47 @@ import {
     getIntentName,
 } from 'ask-sdk-core';
 
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+
+// let speakOutput: Promise<string>;
+
+const axiosCall = (): Promise<string> => {
+    let speakOutput = 'Here\'s what you can do: ';
+    return new Promise((resolve, reject) => {
+        axios.get('https://raw.githubusercontent.com/rodriguez/personal-json-apis/main/db.json')
+            .then(
+                (response: AxiosResponse): void => {
+                    const body: { options: [{ label: string, value: string }] } = response.data;
+                    console.log(body);
+                    const wordArray: string[] = body.options.map((obj: { label: string, value: string }) => obj.value);
+                    console.log(wordArray.join(', '));
+                    speakOutput = speakOutput.concat(wordArray.join(', '));
+                    resolve(speakOutput);
+                },
+                (error: AxiosError): void => {
+                    reject('This resulted in an error.');;
+                }
+
+            );
+    })
+}
 
 export const MainRequestHandler: RequestHandler = {
     canHandle(handlerInput: HandlerInput) {
         return getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' && getIntentName(handlerInput.requestEnvelope) === 'Main';
     },
-    handle(handlerInput: HandlerInput) {
-        let speakOutput = 'Here\'s what you can do: ';
-        axios.get('https://raw.githubusercontent.com/rodriguez/personal-json-apis/main/db.json')
-            .then((response: AxiosResponse): void => {
-                const body: { options: [{ label: string, value: string }] } = response.data;
-                const wordArray: string[] = body.options.map((obj: { label: string, value: string }) => obj.value);
-                speakOutput = speakOutput.concat(wordArray.join(', '))
-            })
+    async handle(handlerInput: HandlerInput) {
+        // let speakOutput = 'Here\'s what you can do: ';
+        // axios.get('https://raw.githubusercontent.com/rodriguez/personal-json-apis/main/db.json')
+        //     .then((response: AxiosResponse): void => {
+        //         const body: { options: [{ label: string, value: string }] } = response.data;
+        //         const wordArray: string[] = body.options.map((obj: { label: string, value: string }) => obj.value);
+        //         speakOutput = speakOutput.concat(wordArray.join(', '))
+        // });
+        const response: string = await axiosCall();
         return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
+            .speak(response)
+            .reprompt(response)
             .getResponse();
     }
 };
